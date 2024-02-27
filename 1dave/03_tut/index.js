efe5f4dc796fabc9fc67f3a29099050f56876071
Starter <starter@Starters-MacBook-Pro.local>
2024-02-27 16:17:34 -0600

    more npm modules practice; commonjs syntax

--- 1dave/03_tut/index.js
+++ 1dave/03_tut/index.js
@@ -1,4 +1,20 @@
 const { format } = require('date-fns');
+// destructuring with CommonJS
+const { v4: uuid } = require('uuid');
+// compared too es6 import
+// import v4 as uuid;
 
-console.log(format(new Date(), 'yyyy-MM-dd\tHH:mm:ss'));
+
+console.log(format(new Date(), 'yyyy-MM\tHH:mm:ss'));
+console.log(format(new Date(), 'MM-dd\tHH:mm:ss'));
+console.log(format(new Date(), 'ddMM\tHH:mm:ss'));
+// only adding this line to test nodemon
 console.log("Hello")
+
+console.log(uuid());
+// prints a new id every refresh
+
+
+// dev-dependecy
+// npm i package -D
+
